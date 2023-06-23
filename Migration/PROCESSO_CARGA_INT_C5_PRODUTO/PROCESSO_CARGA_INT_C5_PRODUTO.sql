be7162e80b2094322baaa3eb182588a7ebcc0dd1
igor.martins <igor;martins@totvs.com.br>
2023-06-23 16:00:43 -0300

    Adicionado view marca no migration de produto

--- Migration/PROCESSO_CARGA_INT_C5_PRODUTO/PROCESSO_CARGA_INT_C5_PRODUTO.sql
+++ Migration/PROCESSO_CARGA_INT_C5_PRODUTO/PROCESSO_CARGA_INT_C5_PRODUTO.sql
@@ -1,3 +1,16 @@
+CREATE OR REPLACE VIEW VW_INT_C5_MARCA AS
+(
+SELECT  m.codmarca seqmarca,
+        SUBSTR(m.marca,1,20) marca,
+        COALESCE(m.ativo,'S') ativo,
+        m.dtultalter,
+        m.dtcadastro
+  FROM  pcmarca m
+ WHERE  m.codmarca > 0
+ )
+
+ \
+
 CREATE OR REPLACE VIEW VW_INT_C5_FAMILIA AS
 (
   SELECT DISTINCT
