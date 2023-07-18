6e2c18a8ddfbc02fbe8586f34fdb4812b25060ff
tp.danilo.Pacheco <tp.danilo.pacheco@totvspartners.com.br>
2023-07-18 14:27:28 -0300

    Criação de view para condicao de pagamento

---
+++ View/VW_INT_C5_PLPAG/VW_INT_C5_PLPAG.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE VIEW VW_INT_C5_PLPAG AS
+(
+  SELECT
+    pcplpag.codplpag NROCONDICAOPAGTO,
+    SUBSTR(pcplpag.descricao,1,40) CONDICAOPAGTO,
+    NVL(pcplpag.pertxfim,0) PERCACRESCIMO,
+    (CASE WHEN pcplpag.formaparcelamento = 'T' THEN  
+          pcplpag.numeroparcelasdiafixo WHEN pcplpag.formaparcelamento = 'V' THEN 
+          pcplpag.numparcelas 
+     ELSE 0 END) NROMAXIMOPARCELA,
+    pcplpag.numdias NRODIASVENCTO,
+    pcplpag.status ATIVO
+  FROM PCPLPAG
+  WHERE NVL(pcplpag.usaplpagautoservico,'N') = 'S'
+);
