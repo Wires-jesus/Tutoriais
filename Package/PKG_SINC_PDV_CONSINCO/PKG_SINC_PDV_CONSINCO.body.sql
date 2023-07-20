c08aed9d08245dd5917345d33df21a66f4013d12
tp.caio.souza <tp.caio.souza@totvspartners.com.br>
2023-07-20 17:25:17 -0300

    correcao package

--- Package/PKG_SINC_PDV_CONSINCO/PKG_SINC_PDV_CONSINCO.body.sql
+++ Package/PKG_SINC_PDV_CONSINCO/PKG_SINC_PDV_CONSINCO.body.sql
@@ -1842,7 +1842,6 @@ CREATE OR REPLACE PACKAGE BODY PKG_SINC_PDV_CONSINCO IS
       ON (s.codnbmsh = b.codnbmsh)
       WHEN MATCHED THEN
       UPDATE SET
-               s.idref                 = b.codfilial,
                s.ufdestino             = b.ufdestino,
                s.perctributoimportado  = b.perctributoimportado,
                s.perctributonacfederal = b.perctributonacfederal,
@@ -1854,7 +1853,6 @@ CREATE OR REPLACE PACKAGE BODY PKG_SINC_PDV_CONSINCO IS
         
       WHEN NOT MATCHED THEN
         INSERT (s.codnbmsh,
-                s.idref,
                 s.ufdestino,
                 s.perctributoimportado,
                 s.perctributonacfederal,
@@ -1865,7 +1863,6 @@ CREATE OR REPLACE PACKAGE BODY PKG_SINC_PDV_CONSINCO IS
                 s.ativo)
                 VALUES
                   (b.codnbmsh,
-                   b.codfilial,
                    b.ufdestino,
                    b.perctributoimportado,
                    b.perctributonacfederal,
