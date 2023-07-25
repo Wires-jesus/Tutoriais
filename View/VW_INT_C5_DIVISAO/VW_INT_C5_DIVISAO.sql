3444fcb9ff5fc7dd0fa52947e31de15d71cce683
igor.martins <igor;martins@totvs.com.br>
2023-07-25 09:37:20 -0300

    Alteracao view divisao

---
+++ View/VW_INT_C5_DIVISAO/VW_INT_C5_DIVISAO.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE VIEW VW_INT_C5_DIVISAO AS
+(
+SELECT PCREGIAO.NUMREGIAO NRODIVISAO,
+       PCREGIAO.REGIAO DIVISAO,
+       'V' TIPO,
+       (CASE
+           WHEN NVL(PCREGIAO.STATUS, 'A') = 'A' THEN
+                'S'
+           ELSE 'N'  
+        END) ATIVO,
+        PCREGIAO.UF IDREF
+FROM PCREGIAO,
+     (select s.ultimaexecucao from pccontroleconsinco s where upper(s.objetoreferencia) = 'PKG_SINC_PDV_CONSINCO.CARREGA_TB_DIVISAO') DTPADRAO
+ WHERE NVL(PCREGIAO.DTALTERC5, DTPADRAO.ULTIMAEXECUCAO)  >= DTPADRAO.ULTIMAEXECUCAO
+
+)
