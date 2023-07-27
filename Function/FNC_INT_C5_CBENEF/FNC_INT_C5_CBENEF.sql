a02a4e78a8c87eab936245b4ec45d7b6bd0d256b
igor.martins <igor;martins@totvs.com.br>
2023-07-27 09:23:36 -0300

    Add function cBenef

---
+++ Function/FNC_INT_C5_CBENEF/FNC_INT_C5_CBENEF.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION FNC_INT_C5_CBENEF(pUF         CHAR,
+                                             pCFOP       NUMBER,
+                                             pSittribut  VARCHAR2)
+  RETURN VARCHAR2
+  IS
+    vCBenef  VARCHAR2(10);
+  BEGIN
+
+    SELECT  v.codigobeneficio
+      INTO  vCBenef
+      FROM  pccodigobeneficiofiscalvinculo v,
+            pccodigobeneficiofiscal        c
+     WHERE  v.codigobeneficio = c.codigobeneficio
+       AND  v.ufdestino = pUF
+       AND  v.sittribut = pSittribut
+       AND  v.codfiscal = pCFOP;
+    
+    RETURN(vCBenef);
+END;
