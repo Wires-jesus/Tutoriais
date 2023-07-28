a7d4c4fe71e27f485df43f7d6e82fd4995edb90c
igor.martins <igor;martins@totvs.com.br>
2023-07-28 09:18:46 -0300

    Ajuste view empresa

--- View/VW_INT_C5_EMPRESA/VW_INT_C5_EMPRESA.sql
+++ View/VW_INT_C5_EMPRESA/VW_INT_C5_EMPRESA.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE VIEW VW_INT_C5_EMPRESA AS
 (
 SELECT f.codigo nroempresa,
        NVL(f.codcli,1) seqpessoa,
-       ferramentas.f_buscarparametro_num('NUMREGIAOPADRAOVAREJO',f.codigo, 1) nrodivisao,
+       ferramentas.f_buscarparametro_num('NUMREGIAOPADRAOVAREJO',f.codigo, '1') nrodivisao,
        1 nrosegmento,
        SUBSTR(COALESCE(f.fantasia, F.RAZAOSOCIAL, ' '), 1, 20) nomereduzido,
        f.codigo nroempresamatriz,
@@ -15,10 +15,10 @@ SELECT f.codigo nroempresa,
             ELSE
           'N'
         END) ativo,
-        (SELECT UF FROM PCREGIAO WHERE NUMREGIAO = ferramentas.f_buscarparametro_num('NUMREGIAOPADRAOVAREJO', f.codigo, 1) ) idref
+        (SELECT UF FROM PCREGIAO WHERE NUMREGIAO = ferramentas.f_buscarparametro_num('NUMREGIAOPADRAOVAREJO', f.codigo, '1') ) idref
   FROM  pcfilial f,
        (select s.ultimaexecucao from pccontroleconsinco s where upper(s.objetoreferencia) = 'PKG_SINC_PDV_CONSINCO.CARREGA_TB_EMPRESA') DTPADRAO
- WHERE f.codigo >= 0
+ WHERE f.codigo >= '0'
    AND f.codigo < '99'
    AND NVL(F.Dtalterc5, DTPADRAO.ULTIMAEXECUCAO)  >= DTPADRAO.ULTIMAEXECUCAO
 
