33fd800eb25b1cd804e3e26a395b90446b6957ab
igor.martins <igor;martins@totvs.com.br>
2023-07-28 16:30:11 -0300

    Ajuste lei transp

--- View/VW_INT_C5_LEITRANSP/VW_INT_C5_LEITRANSP.sql
+++ View/VW_INT_C5_LEITRANSP/VW_INT_C5_LEITRANSP.sql
@@ -1,36 +1,53 @@
 CREATE OR REPLACE VIEW VW_INT_C5_LEITRANSP AS
-SELECT DISTINCT
+SELECT 
+  CARGATRIB.*
+FROM
+  (SELECT DISTINCT
        NCMFILIAL.CODNCM codncmsh,
        NVL(NCMFILIAL.CODEX, 0) ex,
        NCMFILIAL.UF ufdestino,
-       
+
        NVL((SELECT PERCTRIBUTOS
-            FROM PCTRIBUT 
-            WHERE CODST = (select distinct t.codst 
-                           from pctabpr t, pcprodut p, pcregiao r 
-                           where t.codprod = p.codprod
-                           and   t.numregiao = r.numregiao
-                           and   r.uf = NCMFILIAL.UF
-                           and   p.codncmex = NCMFILIAL.CODNCM||'.'||NCMFILIAL.CODEX AND ROWNUM = 1)), 0) perctributos,
-        
+        FROM PCTRIBUT
+        WHERE CODST = (select distinct t.codst
+                       from pctabpr t, pcprodut p, pcregiao r
+                       where t.codprod = p.codprod
+                       and   t.numregiao = r.numregiao
+                       and   r.uf = NCMFILIAL.UF
+                       and   p.codncmex = NCMFILIAL.CODNCM||'.'||NCMFILIAL.CODEX AND ROWNUM = 1)), 0) perctributos,
+       
+       GREATEST(
+                (SELECT DTALTERC5
+                 FROM PCTRIBUT
+                 WHERE CODST = (select distinct t.codst
+                                from pctabpr t, pcprodut p, pcregiao r
+                                where t.codprod = p.codprod
+                                and   t.numregiao = r.numregiao
+                                and   r.uf = NCMFILIAL.UF
+                                and   p.codncmex = NCMFILIAL.CODNCM||'.'||NCMFILIAL.CODEX AND ROWNUM = 1)),
+                                
+                  NCMFILIAL.dtalterc5,
+                  TRIBNCMFILIAL.dtalterc5
+                )dtalterc5,  
+
         NVL(TRIBNCMFILIAL.percentfisica, 0) perctributonacfederal,
         NVL(TRIBNCMFILIAL.percfisicaestimp, 0) perctributoimportado,
         NVL(TRIBNCMFILIAL.percentfisicaimportado, 0) perctributoimpfederal,
         NVL(TRIBNCMFILIAL.percfisicaestnac, 0) perctributoestadual,
         NVL(TRIBNCMFILIAL.percfisicamunicnac, 0) perctributomunicipal,
-       
+
        (CASE
            WHEN NCMFILIAL.DTEXCLUSAO IS NULL THEN
-                'S' 
+                'S'
            ELSE
                 'N'
        END) ativo
-FROM 
-    (SELECT 
+   FROM
+    (SELECT
        NCM.*,
        (SELECT CODIGO FROM PCFILIAL WHERE UF = NCM.UF AND REGEXP_LIKE(CODIGO, '^[[:digit:]]+$') AND CODIGO <> '99' AND ROWNUM = 1) CODFILIAL
      FROM
-         (SELECT DISTINCT 
+         (SELECT DISTINCT
              PCNCM.CODNCM,/*CODNCM É CADASTRADO SEM O "." */
              PCNCM.CODNCMEX,/*CODNCM DA PCNCM É CADASTRADO COM "." E JÁ POSSUI A EXCEÇÃO CASO EXISTA*/
              PCNCM.CODEX,
@@ -38,30 +55,38 @@ FROM
              PCNCM.DTALTERC5,
              PCFILIAL.UF
            FROM PCNCM,
-                PCFILIAL 
+                PCFILIAL
            WHERE PCFILIAL.CODIGO <> '99'
            AND   REGEXP_LIKE(PCFILIAL.CODIGO, '^[[:digit:]]+$')
            AND   PCFILIAL.UF IS NOT NULL
            AND   PCNCM.CODNCMEX > 0
            )NCM
      )NCMFILIAL,
-     
-    (SELECT 
+
+    (SELECT
        PCTRIBNCMFILIAL.codncm||PCTRIBNCMFILIAL.codfilial ID,
+       PCTRIBNCMFILIAL.DTALTERC5,
        PCTRIBNCMFILIAL.percentfisica,
-       PCTRIBNCMFILIAL.percfisicaestimp, 
-       PCTRIBNCMFILIAL.percentfisicaimportado, 
-       PCTRIBNCMFILIAL.percfisicaestnac, 
+       PCTRIBNCMFILIAL.percfisicaestimp,
+       PCTRIBNCMFILIAL.percentfisicaimportado,
+       PCTRIBNCMFILIAL.percfisicaestnac,
        PCTRIBNCMFILIAL.percfisicamunicnac
      FROM PCTRIBNCMFILIAL
      WHERE PCTRIBNCMFILIAL.CODFILIAL <> '99'
      AND   REGEXP_LIKE(PCTRIBNCMFILIAL.CODFILIAL, '^[[:digit:]]+$')
      )TRIBNCMFILIAL,
-     
+
      (select min(s.ultimaexecucao) ultimaexecucao
       from pccontroleconsinco s
       where (upper(s.objetoreferencia) = 'PKG_SINC_PDV_CONSINCO.CARREGA_TB_CARGATRIBUTARIA')
      ) DTPADRAO
-          
-WHERE NCMFILIAL.CODNCMEX||NCMFILIAL.CODFILIAL = TRIBNCMFILIAL.ID(+)
-AND NVL(NCMFILIAL.DTALTERC5, DTPADRAO.ULTIMAEXECUCAO) >= DTPADRAO.ULTIMAEXECUCAO
+
+   WHERE NCMFILIAL.CODNCMEX||NCMFILIAL.CODFILIAL = TRIBNCMFILIAL.ID(+)
+   )CARGATRIB,
+     
+   (select min(s.ultimaexecucao) ultimaexecucao
+    from pccontroleconsinco s
+    where (upper(s.objetoreferencia) = 'PKG_SINC_PDV_CONSINCO.CARREGA_TB_CARGATRIBUTARIA')
+   )DTPADRAO
+
+WHERE NVL(CARGATRIB.DTALTERC5, DTPADRAO.ULTIMAEXECUCAO) >= DTPADRAO.ULTIMAEXECUCAO
