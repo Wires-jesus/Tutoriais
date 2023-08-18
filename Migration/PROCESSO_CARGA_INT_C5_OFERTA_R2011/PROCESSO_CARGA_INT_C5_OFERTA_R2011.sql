4031157a533aa64ab7a461deda37563af47c08af
tp.caio.souza <tp.caio.souza@totvspartners.com.br>
2023-08-17 21:25:03 -0300

    correcao PR

--- Migration/PROCESSO_CARGA_INT_C5_OFERTA_R2011/PROCESSO_CARGA_INT_C5_OFERTA_R2011.sql
+++ Migration/PROCESSO_CARGA_INT_C5_OFERTA_R2011/PROCESSO_CARGA_INT_C5_OFERTA_R2011.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE VIEW VW_INT_C5_OFERTA_R2011 AS
 (
- select codfilial||2011||codoferta as SEQREGRA,
+select codfilial||2011||codoferta as SEQREGRA,
        descoferta                  as regra,
        3                           as SEQTIPOCREDITO,
        (CASE
@@ -12,9 +12,11 @@ CREATE OR REPLACE VIEW VW_INT_C5_OFERTA_R2011 AS
        DTINICIAL                   as dtahorinicio,
        DTFINAL                     as dtahorfim
   from PCOFERTAPROGRAMADAC A,
-  (SELECT S.ULTIMAEXECUCAO
-          FROM PCCONTROLECONSINCO S
-    WHERE UPPER(S.OBJETOREFERENCIA) = 'PKG_SINC_PDV_CONSINCO.CARREGA_TB_REGRAINCENTIVO') DATAPADRAO
+  (select min(s.ultimaexecucao) ultimaexecucao
+        from pccontroleconsinco s
+        where (upper(s.objetoreferencia) = 'PKG_SINC_PDV_CONSINCO.CARREGA_TB_REGRAINCENTIVO')
+           or (upper(s.objetoreferencia) = 'PKG_SINC_PDV_CONSINCO.CARREGA_TB_REGRAINCENTPERIODO')
+ ) DATAPADRAO
   WHERE NVL(A.DTALTERC5, DATAPADRAO.ULTIMAEXECUCAO) >= DATAPADRAO.ULTIMAEXECUCAO
 )
 
