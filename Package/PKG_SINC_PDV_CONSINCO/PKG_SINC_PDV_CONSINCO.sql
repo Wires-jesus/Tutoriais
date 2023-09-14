4625f0ab00cd8065b57744d2956aaa55f7d99a96
igor.martins <igor;martins@totvs.com.br>
2023-09-13 23:04:13 -0300

    Ajustes ofertas atacado 2011 e 2017

--- Package/PKG_SINC_PDV_CONSINCO/PKG_SINC_PDV_CONSINCO.sql
+++ Package/PKG_SINC_PDV_CONSINCO/PKG_SINC_PDV_CONSINCO.sql
@@ -11,6 +11,8 @@ CREATE OR REPLACE PACKAGE PKG_SINC_PDV_CONSINCO IS
 
   FUNCTION get_inicio_execucao RETURN TIMESTAMP;
 
+  FUNCTION obter_seqapartirde RETURN NUMBER;
+
   PROCEDURE atualiza_sinc_processo(p_id IN pccontroleconsinco.id%TYPE);
 
   PROCEDURE carrega_tb_pessoa(p_id IN pccontroleconsinco.id%TYPE);
