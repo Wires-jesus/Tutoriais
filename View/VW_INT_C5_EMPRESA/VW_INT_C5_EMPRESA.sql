92762f5c1ee2fe2abc75f0cd6c4bbac2dfdfa10d
pedrohe.silva <pedrohe.silva@totvs.com.br>
2023-09-20 19:34:54 -0300

    Ajuste

--- View/VW_INT_C5_EMPRESA/VW_INT_C5_EMPRESA.sql
+++ View/VW_INT_C5_EMPRESA/VW_INT_C5_EMPRESA.sql
@@ -2,14 +2,14 @@ CREATE OR REPLACE VIEW VW_INT_C5_EMPRESA AS
 (
 SELECT f.codigo nroempresa,
        NVL(f.codcli,1) seqpessoa,
-       
+
        (SELECT R.NRODIVISAO
         FROM   PCDEPARAREGIAOC5 R
-        WHERE  R.NUMREGIAO = ferramentas.f_buscarparametro_num('NUMREGIAOPADRAOVAREJO', 
-                                                               F.CODIGO, 
-                                                              '1') 
+        WHERE  R.NUMREGIAO = ferramentas.f_buscarparametro_num('NUMREGIAOPADRAOVAREJO',
+                                                               F.CODIGO,
+                                                              '1')
        )NRODIVISAO,
-       
+
        --ferramentas.f_buscarparametro_num('NUMREGIAOPADRAOVAREJO',f.codigo, '1') nrodivisao,
        1 nrosegmento,
        SUBSTR(COALESCE(f.fantasia, F.RAZAOSOCIAL, ' '), 1, 20) nomereduzido,
@@ -27,9 +27,11 @@ SELECT f.codigo nroempresa,
   FROM  pcfilial f,
        (select min(s.ultimaexecucao) ultimaexecucao from pccontroleconsinco s
         where upper(s.objetoreferencia) = 'PKG_SINC_PDV_CONSINCO.CARREGA_TB_EMPRESA'
-        or upper(s.objetoreferencia) = 'PKG_SINC_PDV_CONSINCO.CARREGA_TB_EMPRESASEGMENTO') DTPADRAO
+        or upper(s.objetoreferencia) = 'PKG_SINC_PDV_CONSINCO.CARREGA_TB_EMPRESASEGMENTO') DTPADRAO,
+        VW_INT_C5_OBTER_FILIAIS_C5 c5
  WHERE f.codigo >= '0'
    AND f.codigo < '99'
+   AND f.codigo = c5.codfilial
    AND NVL(F.Dtalterc5, DTPADRAO.ULTIMAEXECUCAO)  >= DTPADRAO.ULTIMAEXECUCAO
-
 )
+
