49f09950ffa2cd7ed28cbeca34b2d140ee22802a
Jose Carlos Vaz Felipe <jose.vfelipe@totvs.com.br>
2023-10-04 08:22:48 -0300

    Subindo FNC_INT_C5_NUMPEDECF para buscar o numpedecf a partir da subida de venda C5

---
+++ Function/FNC_INT_C5_NUMPEDECF/FNC_INT_C5_NUMPEDECF.sql
@@ -0,0 +1,47 @@
+CREATE OR REPLACE FUNCTION FNC_INT_C5_NUMPEDECF(pSEQDOCTO NUMBER,
+                                                pNroCheckout NUMBER,
+                                                pNroEmpresa NUMBER) 
+  RETURN NUMBER
+IS
+    vNumpedecf NUMBER;
+    vClob CLOB;
+    vXMLDoc XMLTYPE;
+BEGIN
+  vNumpedecf := -1;
+  BEGIN
+   SELECT A.MENSAGEM
+   INTO vClob
+   FROM
+   (SELECT M.MENSAGEM as MENSAGEM
+     FROM PCFILAMENSAGEM M
+    WHERE M.SEQDOCTO = pSEQDOCTO
+      AND M.NUMCAIXA = pNroCheckout
+      AND M.CODFILIAL = pNroEmpresa
+      AND M.TIPOOPERACAO = 'VEND'
+    UNION ALL
+     SELECT MH.MENSAGEM as MENSAGEM
+     FROM PCFILAMENSAGEMHISTORICO MH
+    WHERE MH.SEQDOCTO = pSEQDOCTO
+      AND MH.NUMCAIXA = TO_CHAR(pNroCheckout)
+      AND MH.CODFILIAL = pNroEmpresa
+      AND MH.TIPOOPERACAO = 'VEND'
+    UNION ALL
+     SELECT ME.MENSAGEM as MENSAGEM
+     FROM PCFILAMENSAGEMERRO ME
+    WHERE ME.SEQDOCTO = pSEQDOCTO
+      AND ME.NUMCAIXA = pNroCheckout
+      AND ME.CODFILIAL = pNroEmpresa
+      AND ME.TIPOOPERACAO = 'VEND') A;
+  EXCEPTION
+    WHEN OTHERS THEN
+    vNumpedecf := 0;   
+  END; 
+   
+  IF (vNumpedecf < 0) THEN
+    vXMLDoc := XMLType.createXML(vClob);
+    vNumpedecf := TO_NUMBER(vXMLDoc.extract('/EsquemaExportacao/Pedido/Pedido/Numpedecf/text()').getStringVal());
+  END IF; 
+
+    
+ RETURN(vNumpedecf);
+END;
