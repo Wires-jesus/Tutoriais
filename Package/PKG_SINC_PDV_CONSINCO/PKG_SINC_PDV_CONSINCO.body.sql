dbe20a9395ec3db1a1d410108e42eba3cb367d58
igor.martins <igor.martins@totvs.com.br>
2023-10-31 15:02:19 -0300

    Ajuste origem mercadoria

--- Package/PKG_SINC_PDV_CONSINCO/PKG_SINC_PDV_CONSINCO.body.sql
+++ Package/PKG_SINC_PDV_CONSINCO/PKG_SINC_PDV_CONSINCO.body.sql
@@ -2270,7 +2270,7 @@ CREATE OR REPLACE PACKAGE BODY PKG_SINC_PDV_CONSINCO IS
   PROCEDURE carrega_tb_famdivisao(p_id IN pccontroleconsinco.id%TYPE) AS
   BEGIN
     MERGE INTO monitorpdvmiddle.tb_famdivisao s
-        USING (SELECT DISTINCT 
+        USING (SELECT  
                      E.seqfamilia,
                      E.nrodivisao,
                      E.nrotributacao,
