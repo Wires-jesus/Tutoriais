31f4608e1a1675bf8a45e0e15fef144d3950658c
igor.martins <igor.martins@totvs.com.br>
2023-12-07 11:06:48 -0300

    Ajuste sequencial

--- Migration/PROCESSO_CARGA_INT_C5_SEQUENCE/PROCESSO_CARGA_INT_C5_SEQUENCE.sql
+++ Migration/PROCESSO_CARGA_INT_C5_SEQUENCE/PROCESSO_CARGA_INT_C5_SEQUENCE.sql
@@ -1,6 +1,30 @@
-CREATE SEQUENCE DFSEQ_INT_C5_PRODPRECOAPARTIR
-                minvalue 1
-                maxvalue 999999999999999999999999999
-                start with 1
-                increment by 1
-                cache 20
+DECLARE
+  vnContSeq NUMBER;
+  vScript VARCHAR2(4000);
+BEGIN
+
+  BEGIN
+    SELECT COUNT(1)
+    INTO vnContSeq
+    FROM ALL_SEQUENCES A
+    WHERE A.SEQUENCE_NAME = 'DFSEQ_INT_C5_PRODPRECOAPARTIR';
+  END;
+     
+  IF vnContSeq = 0  THEN
+     vScript := ' CREATE SEQUENCE DFSEQ_INT_C5_PRODPRECOAPARTIR minvalue 1 maxvalue 999999999999999999999999999 start with 1 increment by 1 nocache';
+     execute immediate vscript;  
+  END IF;
+    
+  BEGIN
+    SELECT COUNT(1)
+    INTO vnContSeq
+    FROM ALL_SEQUENCES A
+    WHERE A.SEQUENCE_NAME = 'DFSEQ_INT_C5_REGRAINCENTIVO';
+  END;
+     
+  IF vnContSeq = 0  THEN
+     vScript := ' CREATE SEQUENCE DFSEQ_INT_C5_REGRAINCENTIVO minvalue 1 maxvalue 999999999999999999999999999 start with 1 increment by 1 nocache';
+     execute immediate vscript;  
+  END IF;
+  
+END;                
