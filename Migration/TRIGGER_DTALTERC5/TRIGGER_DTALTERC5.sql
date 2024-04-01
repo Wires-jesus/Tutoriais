871e30356ff05274357ec0ccc4d9ac45cb51ce2d
igor.martins <igor.martins@totvs.com.br>
2024-04-01 12:26:36 -0300

    Ajuste cesta e kit

--- Migration/TRIGGER_DTALTERC5/TRIGGER_DTALTERC5.sql
+++ Migration/TRIGGER_DTALTERC5/TRIGGER_DTALTERC5.sql
@@ -5,16 +5,6 @@ REFERENCING NEW AS NEW OLD AS OLD
  FOR EACH ROW
 BEGIN
     :NEW.DTALTERC5 := CURRENT_TIMESTAMP;
-
-    /*IF (:OLD.PERCTRIBUTOS <> :NEW.PERCTRIBUTOS) THEN
-       UPDATE PCNCM SET
-              DTALTERC5 = CURRENT_TIMESTAMP
-       WHERE CODNCM IN (SELECT DISTINCT P.NBM 
-                         FROM PCPRODUT P,
-                              PCTABPR R 
-                         WHERE P.CODPROD = R.CODPROD
-                         AND   R.CODST = :OLD.CODST);   
-    END IF;*/
 END;
 
 \
