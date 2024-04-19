10bfe59ea7536e15f17f76780df901c696afcdc9
igor.martins <igor.martins@totvs.com.br>
2024-04-19 14:39:33 -0300

    Ajuste regra produto

--- Migration/PROCESSO_CARGA_INT_C5_REGISTROS/PROCESSO_CARGA_INT_C5_REGISTROS.sql
+++ Migration/PROCESSO_CARGA_INT_C5_REGISTROS/PROCESSO_CARGA_INT_C5_REGISTROS.sql
@@ -177,11 +177,14 @@ values (33, 'tb_regrasegmento')
 insert into pctipoprocessoconsinco(ID, DESCRICAO)
 values (34, 'tb_regraproduto')
 
-/*
-\
 
-insert into pctipoprocessoconsinco(ID, DESCRICAO)
-values (35, 'tb_regrafamilia')*/
+
+--\
+
+--insert into pctipoprocessoconsinco(ID, DESCRICAO)
+--values (35, 'tb_regrafamilia')
+
+
 
 \
 
@@ -476,11 +479,12 @@ values (33, 33, 'Sincronização de tabela TB_REGRASEGMENTO', TRUNC(SYSDATE), 'D
 insert into pccontroleconsinco(ID, CODPROCESSO, DESCRICAO, ULTIMAEXECUCAO, TIPO, OBJETOREFERENCIA, PRECEDENCIA, ATIVO, DTCRIACAO, DTALTERACAO)
 values (34, 34, 'Sincronização de tabela TB_REGRAPRODUTO', TRUNC(SYSDATE), 'D', 'pkg_sinc_PDV_Consinco.carrega_tb_regraproduto',  34,  'A',  TRUNC(SYSDATE),  TRUNC(SYSDATE) )
 
-/*
-\
 
-insert into pccontroleconsinco(ID, CODPROCESSO, DESCRICAO, ULTIMAEXECUCAO, TIPO, OBJETOREFERENCIA, PRECEDENCIA, ATIVO, DTCRIACAO, DTALTERACAO)
-values (35, 35, 'Sincronização de tabela TB_REGRAFAMILIA', TRUNC(SYSDATE), 'D', 'pkg_sinc_PDV_Consinco.carrega_tb_regrafamilia',  35,  'A',  TRUNC(SYSDATE),  TRUNC(SYSDATE) )*/
+--\
+
+--insert into pccontroleconsinco(ID, CODPROCESSO, DESCRICAO, ULTIMAEXECUCAO, TIPO, OBJETOREFERENCIA, PRECEDENCIA, ATIVO, DTCRIACAO, DTALTERACAO)
+--values (35, 35, 'Sincronização de tabela TB_REGRAFAMILIA', TRUNC(SYSDATE), 'D', 'pkg_sinc_PDV_Consinco.carrega_tb_regrafamilia',  35,  'A',  TRUNC(SYSDATE),  TRUNC(SYSDATE) )
+
 
 \
 
