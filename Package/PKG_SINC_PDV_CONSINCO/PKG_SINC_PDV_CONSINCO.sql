007ea47ee1510f4ce85007cd9159bcb26a059590
pedrohe.silva <pedrohe.silva@totvs.com.br>
2024-07-15 16:54:53 -0300

    Adcionando carga na package

--- Package/PKG_SINC_PDV_CONSINCO/PKG_SINC_PDV_CONSINCO.sql
+++ Package/PKG_SINC_PDV_CONSINCO/PKG_SINC_PDV_CONSINCO.sql
@@ -145,6 +145,8 @@ CREATE OR REPLACE PACKAGE PKG_SINC_PDV_CONSINCO IS
   
   procedure carrega_tb_precoapartirempresa(p_id in pccontroleconsinco.id%type);
 
+  procedure carrega_tb_precoapartirsegmento(p_id in pccontroleconsinco.id%type);
+
   PROCEDURE exec_sinc;
 
   --PROCEDURE exec_sinc_PRECO;
