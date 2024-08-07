17fc87655144d3ff6b13c4e0ba45091ddd98fb13
rodrigo.velez <rodrigo.velez@totvs.com.br>
2024-08-07 15:50:15 -0300

    DENGWTNG-4545 População da tabela PCPARAMFILIAL com dados iniciais.

--- Migration/TABELA_PCPARAMNPS/TABELA_PCPARAMNPS.sql
+++ Migration/TABELA_PCPARAMNPS/TABELA_PCPARAMNPS.sql
@@ -5,8 +5,8 @@ INSERT INTO pcparamnps
   data_erro,
   tempo_inatividade)
 VALUES
-  ('XXXXXXXXXX',
-  'XXXXXXXXX',
+  ('https://nps.totvs.io',
+  'AcdS+R93n5Vf8Pkt+DBMY5y25ttZjNxT7MJtxUXbd8q98jyk98+Z+GJ59QQNFTXhDM9WNFzEhfNk2CsJCoUTykvRKWGhF1rScW5ey7UY+FY2OFZgKTHaFK60Jh7bj5LR',
   'S',
   null,
   24);
