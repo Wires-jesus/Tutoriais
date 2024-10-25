d2fdb6d84f94a17af2c23c1b6c34bea1e41e0893
Cassio <cassio.pardim@totvs.com.br>
2024-10-25 17:12:54 -0300

    DDVENDAS-49629 Ajuste na function para verificar se kit pode ser editado

--- Function/FCT_MED_VALIDARKIT/FCT_MED_VALIDARKIT.sql
+++ Function/FCT_MED_VALIDARKIT/FCT_MED_VALIDARKIT.sql
@@ -17,73 +17,134 @@ IS
     
 BEGIN
    vnQTDECOMBO   := 0;
+  BEGIN
+   SELECT PCPROMOCAOMED.TIPOPROMOCAO,PCPROMOCAOMED.TIPOPOLITICA 
+     INTO vvTIPOPROMOCAOMED,vvTIPOLITICAMED
+   FROM  PCPROMOCAOMED 
+   WHERE CODPROMOCAOMED = pnCODPROMOCAOMED;
+  EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+      vvTIPOPROMOCAOMED := NULL;
+      vvTIPOLITICAMED   := NULL;
+  END; 
    
-   FOR CR IN (SELECT CODPROD
-                   , QT 
-                FROM PCPEDI
-               WHERE NUMPED = pnNUMPED
-               ORDER BY NUMSEQ
-                      , CODPROD)
-   LOOP
-      -- BUSCANDO O INTERVALO INICIAL DO PRODUTO NA PROMOÿÿO (KIT)
-      BEGIN
-          SELECT INICIOINTERVALOPROMOCAOMED,PCPROMOCAOMED.TIPOPROMOCAO,PCPROMOCAOMED.TIPOPOLITICA,NVL(PCDESCONTO.QTCOMBOMED,0)
-            INTO vnINICIOINTERVALOPROMOCAOMED,vvTIPOPROMOCAOMED,vvTIPOLITICAMED,vnQTDECOMBOMED
-            FROM PCDESCONTO,PCPROMOCAOMED
-           WHERE PCDESCONTO.CODPROMOCAOMED = PCPROMOCAOMED.CODPROMOCAOMED
-             AND PCDESCONTO.CODPROMOCAOMED = pnCODPROMOCAOMED
-             AND CODPROD = CR.CODPROD
-             AND ROWNUM = 1;
-             
-          -- VERIFICANDO SE TEM RESTO DE DIVISÿO, SE ACHAR E PORQUE O COMBO
-          -- FOI MANIPULADO E NÿO FOI INFORMADO MULTIPLO.
-          IF ((vvTIPOPROMOCAOMED = 'K') AND 
-            (vvTIPOLITICAMED = 'Q') AND 
-            (vnQTDECOMBOMED > 1)) THEN
-            IF (MOD((CR.QT/vnQTDECOMBOMED) , vnINICIOINTERVALOPROMOCAOMED) > 0) THEN
-              vnQTDECOMBO := 0;
-              EXIT;
-            END IF;    
-            
-            -- SE QTDECOMBO FOR ZERO ÿ PQ ÿ O PRIMEIRO ITEM, ENTÿO IRÁ ACHAR A QTDE 
-            -- BASE DO COMBO.
-            IF vnQTDECOMBO = 0 THEN
-              vnQTDECOMBO := (CR.QT/vnQTDECOMBOMED) / vnINICIOINTERVALOPROMOCAOMED;
-            ELSE
-  
-              IF  (vnQTDECOMBO <> ((CR.QT/vnQTDECOMBOMED) / vnINICIOINTERVALOPROMOCAOMED)) THEN
-                 vnQTDECOMBO := 0;
-                 EXIT;
+  IF ((vvTIPOPROMOCAOMED = 'K') AND 
+      (vvTIPOLITICAMED = 'Q'))
+  THEN    
+    FOR CR IN (SELECT CODPROD
+                     , QT,CODDESCONTO
+                  FROM PCPEDI
+                 WHERE NUMPED = pnNUMPED
+                 AND CODPROMOCAOMED = pnCODPROMOCAOMED                 
+                 ORDER BY NUMSEQ
+                        , CODPROD)
+     LOOP
+        -- BUSCANDO O INTERVALO INICIAL DO PRODUTO NA PROMOÿÿO (KIT)
+        BEGIN
+            SELECT INICIOINTERVALOPROMOCAOMED, QTCOMBOMED 
+             INTO vnINICIOINTERVALOPROMOCAOMED,vnQTDECOMBOMED
+            FROM
+            (SELECT INICIOINTERVALOPROMOCAOMED,PCPROMOCAOMED.TIPOPROMOCAO,PCPROMOCAOMED.TIPOPOLITICA,NVL(PCDESCONTO.QTCOMBOMED,0) QTCOMBOMED     
+              FROM PCDESCONTO,PCPROMOCAOMED
+             WHERE PCDESCONTO.CODPROMOCAOMED = PCPROMOCAOMED.CODPROMOCAOMED
+--               AND PCDESCONTO.CODPROMOCAOMED = pnCODPROMOCAOMED
+               AND CODPROD = CR.CODPROD AND CODDESCONTO = CR.CODDESCONTO ORDER BY INICIOINTERVALOPROMOCAOMED )
+               WHERE ROWNUM = 1;
+               
+            -- VERIFICANDO SE TEM RESTO DE DIVISÿO, SE ACHAR E PORQUE O COMBO
+            -- FOI MANIPULADO E NÿO FOI INFORMADO MULTIPLO.
+            IF (vnQTDECOMBOMED > 1) THEN
+              IF (MOD((CR.QT/vnQTDECOMBOMED) , vnINICIOINTERVALOPROMOCAOMED) > 0) THEN
+                vnQTDECOMBO := 0;
+                EXIT;
+              END IF;    
+              
+              -- SE QTDECOMBO FOR ZERO ÿ PQ ÿ O PRIMEIRO ITEM, ENTÿO IRÁ ACHAR A QTDE 
+              -- BASE DO COMBO.
+              IF vnQTDECOMBO = 0 THEN
+                vnQTDECOMBO := (CR.QT/vnQTDECOMBOMED) / vnINICIOINTERVALOPROMOCAOMED;
+              ELSE
+    
+                IF  (vnQTDECOMBO <> ((CR.QT/vnQTDECOMBOMED) / vnINICIOINTERVALOPROMOCAOMED)) THEN
+                   vnQTDECOMBO := 0;
+                   EXIT;
+                END IF;
               END IF;
+            ELSE 
+              
+              IF MOD(CR.QT , vnINICIOINTERVALOPROMOCAOMED) > 0 THEN
+                vnQTDECOMBO := 0;
+                EXIT;
+              END IF;    
+              
+              -- SE QTDECOMBO FOR ZERO ÿ PQ ÿ O PRIMEIRO ITEM, ENTÿO IRÁ ACHAR A QTDE 
+              -- BASE DO COMBO.
+              IF vnQTDECOMBO = 0 THEN
+                vnQTDECOMBO := CR.QT / vnINICIOINTERVALOPROMOCAOMED;
+              ELSE
+                -- A PARTIR DO SEGUNDO ITEM EM DIANTE A QUANTIDADE DE COMBO NÿO PODERÁ 
+                -- SER DIFERENTE DO PRIMEIRO. 
+                IF  vnQTDECOMBO <> (CR.QT / vnINICIOINTERVALOPROMOCAOMED) THEN
+                   vnQTDECOMBO := 0;
+                   EXIT;
+                END IF;
+              END IF;     
             END IF;
-          ELSE 
-            
-            IF MOD(CR.QT , vnINICIOINTERVALOPROMOCAOMED) > 0 THEN
-              vnQTDECOMBO := 0;
-              EXIT;
-            END IF;    
-            
-            -- SE QTDECOMBO FOR ZERO ÿ PQ ÿ O PRIMEIRO ITEM, ENTÿO IRÁ ACHAR A QTDE 
-            -- BASE DO COMBO.
-            IF vnQTDECOMBO = 0 THEN
-              vnQTDECOMBO := CR.QT / vnINICIOINTERVALOPROMOCAOMED;
-            ELSE
-              -- A PARTIR DO SEGUNDO ITEM EM DIANTE A QUANTIDADE DE COMBO NÿO PODERÁ 
-              -- SER DIFERENTE DO PRIMEIRO. 
-              IF  vnQTDECOMBO <> (CR.QT / vnINICIOINTERVALOPROMOCAOMED) THEN
-                 vnQTDECOMBO := 0;
-                 EXIT;
-              END IF;
-            END IF;     
-          END IF;
-             
-      EXCEPTION
-        WHEN OTHERS THEN        
-           vnQTDECOMBO := 0;
-      END;
-   
-   END LOOP;
-   
+               
+        EXCEPTION
+          WHEN OTHERS THEN        
+             vnQTDECOMBO := 0;
+        END;
+     
+     END LOOP;
+  ELSE
+  
+     FOR CR IN (SELECT CODPROD
+                     , QT 
+                  FROM PCPEDI
+                 WHERE  NUMPED = pnNUMPED
+                 AND CODPROMOCAOMED = pnCODPROMOCAOMED   
+                 ORDER BY NUMSEQ
+                        , CODPROD)
+     LOOP
+        -- BUSCANDO O INTERVALO INICIAL DO PRODUTO NA PROMOÿÿO (KIT)
+        BEGIN
+           SELECT INICIOINTERVALOPROMOCAOMED,PCPROMOCAOMED.TIPOPROMOCAO,PCPROMOCAOMED.TIPOPOLITICA,NVL(PCDESCONTO.QTCOMBOMED,0) QTCOMBOMED     
+            INTO vnINICIOINTERVALOPROMOCAOMED,vvTIPOPROMOCAOMED,vvTIPOLITICAMED,vnQTDECOMBOMED
+              FROM PCDESCONTO,PCPROMOCAOMED
+             WHERE PCDESCONTO.CODPROMOCAOMED = PCPROMOCAOMED.CODPROMOCAOMED
+               AND PCDESCONTO.CODPROMOCAOMED = pnCODPROMOCAOMED
+               AND CODPROD = CR.CODPROD
+               AND ROWNUM = 1;
+               
+            -- VERIFICANDO SE TEM RESTO DE DIVISÿO, SE ACHAR E PORQUE O COMBO
+            -- FOI MANIPULADO E NÿO FOI INFORMADO MULTIPLO.
+             IF MOD(CR.QT , vnINICIOINTERVALOPROMOCAOMED) > 0 THEN
+                vnQTDECOMBO := 0;
+                EXIT;
+              END IF;    
+              
+              -- SE QTDECOMBO FOR ZERO ÿ PQ ÿ O PRIMEIRO ITEM, ENTÿO IRÁ ACHAR A QTDE 
+              -- BASE DO COMBO.
+              IF vnQTDECOMBO = 0 THEN
+                vnQTDECOMBO := CR.QT / vnINICIOINTERVALOPROMOCAOMED;
+              ELSE
+                -- A PARTIR DO SEGUNDO ITEM EM DIANTE A QUANTIDADE DE COMBO NÿO PODERÁ 
+                -- SER DIFERENTE DO PRIMEIRO. 
+                IF  vnQTDECOMBO <> (CR.QT / vnINICIOINTERVALOPROMOCAOMED) THEN
+                   vnQTDECOMBO := 0;
+                   EXIT;
+                END IF;
+              END IF;     
+           
+               
+        EXCEPTION
+          WHEN OTHERS THEN        
+             vnQTDECOMBO := 0;
+        END;
+     
+     END LOOP;
+   END IF;
    RETURN vnQTDECOMBO;
 
 END;
