d36bdbbbeef9872987c4911c415f48082cbe0bb1
rodrigo.velez <rodrigo.velez@totvs.com.br>
2025-01-15 10:21:49 -0300

    DENGWTNG-6037 Inclusão de ajuste na tabela de parâmetro mudando a configurãção de 24h para 1min.

--- Migration/TABELA_PCPARAMNPS/TABELA_PCPARAMNPS.sql
+++ Migration/TABELA_PCPARAMNPS/TABELA_PCPARAMNPS.sql
@@ -1,12 +1,26 @@
-INSERT INTO pcparamnps
-  (urlbase,
-  chave_autenticacao,
-  ativo,
-  data_erro,
-  tempo_inatividade)
-VALUES
-  ('https://nps.totvs.io',
-  'AcdS+R93n5Vf8Pkt+DBMY5y25ttZjNxT7MJtxUXbd8q98jyk98+Z+GJ59QQNFTXhDM9WNFzEhfNk2CsJCoUTykvRKWGhF1rScW5ey7UY+FY2OFZgKTHaFK60Jh7bj5LR',
-  'S',
-  null,
-  24)
+DECLARE
+  vQuantidade Integer;
+BEGIN
+  SELECT COUNT(1) INTO vQuantidade FROM pcparamnps;
+  
+  IF (vQuantidade = 0) THEN
+     INSERT INTO pcparamnps
+      (urlbase,
+      chave_autenticacao,
+      ativo,
+      data_erro,
+      tempo_inatividade)
+    VALUES
+      ('https://nps.totvs.io',
+      'AcdS+R93n5Vf8Pkt+DBMY5y25ttZjNxT7MJtxUXbd8q98jyk98+Z+GJ59QQNFTXhDM9WNFzEhfNk2CsJCoUTykvRKWGhF1rScW5ey7UY+FY2OFZgKTHaFK60Jh7bj5LR',
+      'S',
+      null,
+      1);
+  ELSE
+    UPDATE pcparamnps 
+    SET 
+      tempo_inatividade = 1;
+  END IF;
+
+  COMMIT;
+END;
