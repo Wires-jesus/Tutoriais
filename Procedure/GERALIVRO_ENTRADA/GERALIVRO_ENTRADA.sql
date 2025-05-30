3c5a3a518a15e287be2cf7d1429d82c4dbe46482
Messias Vasconcelos dos Santos <tp.messias.vsantos@totvspartners.com.br>
2025-05-30 12:15:58 +0000

    Foi alterado o NUMSQL = 5 no campo Vlr.Desdobrado, incluindo a condição LIKE '%1346%' em um bloco específico que trata o cálculo do Vlr. Frete de forma diferenciada. Para esse tipo de nota fiscal (devolução de cliente), o valor do frete não deve ser somado ao campo Vlr.Desdobrado.

--- Procedure/GERALIVRO_ENTRADA/GERALIVRO_ENTRADA.sql
+++ Procedure/GERALIVRO_ENTRADA/GERALIVRO_ENTRADA.sql
@@ -2060,7 +2060,8 @@ cursor C_NOTAS_DEVOLNFE(P_CODFILIAL in varchar2, P_DATA1 in date, P_DATA2 in dat
            ----------------------------------------------------------------
            CASE
             WHEN (A.ROTINACAD NOT LIKE '%1423%') AND
-                 (A.ROTINACAD NOT LIKE '%1303%')
+                 (A.ROTINACAD NOT LIKE '%1303%') AND
+                 (A.ROTINACAD NOT LIKE '%1346%')
                THEN SUM(ROUND(
                              (NVL(B.QTCONT, 0) * NVL(B.PUNITCONT, 0)) -
                              (NVL(B.QTCONT, 0) * NVL(B.VLIPI, 0)) -
