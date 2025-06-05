649a2bd20b25b753143c5f523e1079773b8d4a22
Jefferson Rodrigues <jefferson.drodrigues@totvs.com.br>
2025-06-05 15:08:25 -0300

    Acrescentado o campo DTPAG ao pagamento de fatura

--- Package/PKG_INT_C5_RECFATURA/PKG_INT_C5_RECFATURA.body.sql
+++ Package/PKG_INT_C5_RECFATURA/PKG_INT_C5_RECFATURA.body.sql
@@ -272,6 +272,7 @@ CREATE OR REPLACE PACKAGE BODY PKG_INT_C5_RECFATURA IS
                                        p.numtranspagdigital AS "Numtranspagdigital",
                                        p.nsupagdigital AS "Nsupagdigital",
                                        p.nomecarteiradigital AS "Nomecarteiradigital",
+                                       p.dtemissao AS "Vpago",
                                        p.carteiradigital AS "Carteiradigital" ))))            
                                  )
                          --  )
