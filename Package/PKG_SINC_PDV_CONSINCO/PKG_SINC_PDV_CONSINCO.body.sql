a892a67b25ba88a519df8a48b7c0b7afb9b46e3c
Jefferson Rodrigues <jefferson.drodrigues@totvs.com.br>
2025-06-16 14:08:45 -0300

    Criação da view VW_INT_C5_CCTCENARIO

--- Package/PKG_SINC_PDV_CONSINCO/PKG_SINC_PDV_CONSINCO.body.sql
+++ Package/PKG_SINC_PDV_CONSINCO/PKG_SINC_PDV_CONSINCO.body.sql
@@ -6595,21 +6595,6 @@ BEGIN
   END;
 END;
 
-PROCEDURE carrega_tb_cctcenario(p_id IN pccontroleconsinco.id%TYPE) AS
-BEGIN
-  MERGE INTO monitorpdvmiddle.tb_cctcenario
-    USING (
-      SELECT 
-        CODIGO_TRIBUTACAO,
-        TIPO_IMPOSTO,
-        TINICIALVALIDADE,
-        DTFINALVALIDADE,
-        TOTALPONTOS
-      FROM VW_INT_C5_CENARIO_TRIBUTACAO
-    )
-
-
-END;
 
 
 PROCEDURE exec_sinc AS
