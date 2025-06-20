3990c2305aedcda6058bacbdcde95ce773a3fc4d
Jose Carlos Vaz Felipe <jose.vfelipe@totvs.com.br>
2025-06-20 12:21:02 -0300

    feita atualização para salvar corretamente os campos DTPAG e VPAGO no processo de recarga celular e pagamento de fatura via PDV Super.

--- Package/PKG_INT_C5_RECFATURA/PKG_INT_C5_RECFATURA.body.sql
+++ Package/PKG_INT_C5_RECFATURA/PKG_INT_C5_RECFATURA.body.sql
@@ -272,7 +272,8 @@ CREATE OR REPLACE PACKAGE BODY PKG_INT_C5_RECFATURA IS
                                        p.numtranspagdigital AS "Numtranspagdigital",
                                        p.nsupagdigital AS "Nsupagdigital",
                                        p.nomecarteiradigital AS "Nomecarteiradigital",
-                                       p.dtemissao AS "Vpago",
+                                       p.dtemissao AS "Dtpag",
+									   p.valor AS "Vpago",
                                        p.carteiradigital AS "Carteiradigital" ))))            
                                  )
                          --  )
