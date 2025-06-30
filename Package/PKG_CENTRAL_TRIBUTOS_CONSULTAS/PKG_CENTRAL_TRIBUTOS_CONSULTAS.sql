f7664be549d3356d0e8c081dd699f33446eebab1
rubens.paulo <rubens.paulo@pcinformatica.com.br>
2025-06-30 16:00:28 -0300

    feature/develop-DDFISCAL-30721-Dev Adição das consultas de entrada de devolução e prefaturamento

--- Package/PKG_CENTRAL_TRIBUTOS_CONSULTAS/PKG_CENTRAL_TRIBUTOS_CONSULTAS.sql
+++ Package/PKG_CENTRAL_TRIBUTOS_CONSULTAS/PKG_CENTRAL_TRIBUTOS_CONSULTAS.sql
@@ -6,20 +6,22 @@ CREATE OR REPLACE PACKAGE PKG_CENTRAL_TRIBUTOS_CONSULTAS AS
                                  P_NUMERONOTA IN NUMBER) IS
     (SELECT N.NUMTRANSVENDA,
             N.CODCLI,
+            NULL NUMTRANSENT,
+            NULL CODFORNEC,
             M.CODPROD,
             M.NUMTRANSITEM,
-            M.PERCICM, -- Alíquota ICMS NF
-            M.SITTRIBUT, -- Situação tributária (Opcional)
-            M.CODFISCAL, -- Código fiscal(CFOP) (Opcional)
-            M.NBM NCM, -- NCM da mercadoria (Opcional)
-            M.PUNITCONT, -- Preço unitário
-            M.VLIPI, -- Valor do IPI
-            M.VLFRETE, -- Valor do frete
-            M.ST VLST, -- Valor do ST
+            M.PERCICM,
+            M.SITTRIBUT,
+            M.CODFISCAL,
+            M.NBM NCM,
+            M.PUNITCONT,
+            M.VLIPI,
+            M.VLFRETE,
+            M.ST VLST,
             MC.VLFECP,
-            M.VLOUTROS, -- Valor de outros
-            M.BASEICMS, -- Base ICMS
-            M.PERCBASERED -- Redução Base ICMS
+            M.VLOUTROS,
+            M.BASEICMS,
+            M.PERCBASERED
        from PCNFSAID       N,
             PCMOV          M,
             PCMOVCOMPLE    MC
@@ -36,6 +38,89 @@ CREATE OR REPLACE PACKAGE PKG_CENTRAL_TRIBUTOS_CONSULTAS AS
         and M.QTCONT > 0
     );
 
+  CURSOR C_DADOS_NF_ENTRADA_DEVOLUCAO(P_CODFILIAL IN VARCHAR2,
+                                     P_NUMEROTRANSACAO IN NUMBER,
+                                     P_NUMERONOTA IN NUMBER) IS
+    (SELECT NULL NUMTRANSVENDA,
+            NULL CODCLI,
+            N.NUMTRANSENT,
+            N.CODFORNEC,
+            M.CODPROD,
+            M.NUMTRANSITEM,
+            M.PERCICM,
+            M.SITTRIBUT,
+            M.CODFISCAL,
+            M.NBM NCM,
+            M.PUNITCONT,
+            M.VLIPI,
+            M.VLFRETE,
+            M.ST VLST,
+            MC.VLFECP,
+            M.VLOUTROS,
+            M.BASEICMS,
+            M.PERCBASERED
+      from PCNFENT N, PCMOV M, PCMOVCOMPLE MC, PCPRODUT P
+     where NVL(N.CODFILIALNF,N.CODFILIAL) = P_CODFILIAL
+       and N.NUMTRANSENT   = P_NUMEROTRANSACAO
+       and N.NUMNOTA       = P_NUMERONOTA
+       and N.NUMTRANSENT   = M.NUMTRANSENT
+       and N.NUMNOTA = M.NUMNOTA
+       AND P.CODPROD = M.CODPROD
+       and N.NUMNOTA = M.NUMNOTA
+       and MC.NUMTRANSITEM(+) = M.NUMTRANSITEM
+       and N.TIPODESCARGA in ('6', '8', 'T')
+       and M.DTCANCEL is null
+       and M.QTCONT > 0
+   );
+   
 
-
+  CURSOR C_DADOS_NF_SAIDA_PREFAT(P_CODFILIAL IN VARCHAR2,
+                                          P_NUMEROTRANSACAO IN NUMBER,
+                                          P_NUMERONOTA IN NUMBER) IS
+    (SELECT N.NUMTRANSVENDA,
+            N.CODCLI,
+            NULL NUMTRANSENT,
+            NULL CODFORNEC,            
+            M.CODPROD,
+            M.NUMTRANSITEM,
+            M.PERCICM,
+            M.SITTRIBUT,
+            M.CODFISCAL,
+            M.NBM NCM,
+            M.PUNITCONT,
+            M.VLIPI,
+            M.VLFRETE,
+            M.ST VLST,
+            MC.VLFECP,
+            M.VLOUTROS,
+            M.BASEICMS,
+            M.PERCBASERED
+      from PCNFSAIDPREFAT       N,
+           PCMOVPREFAT          M,
+           PCMOVCOMPLEPREFAT    MC,
+           PCPRODUT       P,
+           PCCLIENTENDENT ENT,
+           PCCIDADE       CIDADE_ENT,
+           PCPEDC         PD,
+           PCCLIENT       CLIENTE,
+           PCFILIAL       FI
+     where NVL(N.CODFILIALNF,N.CODFILIAL) = P_CODFILIAL
+       and N.NUMTRANSVENDA = P_NUMEROTRANSACAO
+       and N.NUMNOTA       = P_NUMERONOTA
+       and N.NUMTRANSVENDA = M.NUMTRANSVENDA
+       and PD.NUMTRANSVENDA(+) = N.NUMTRANSVENDA
+       and ENT.CODENDENTCLI(+) = NVL(PD.CODENDENTCLI, PD.CODENDENT)
+       and ENT.CODCLI(+) = PD.CODCLI
+       and P.CODPROD = M.CODPROD
+       and N.NUMNOTA = M.NUMNOTA
+       and M.CODOPER <> 'SD'
+       and NVL(N.FINALIDADENFE, 'X') <> 'C'
+       and MC.NUMTRANSITEM(+) = M.NUMTRANSITEM
+       and CIDADE_ENT.CODCIDADE(+) = CLIENTE.CODCIDADECOM
+       and CLIENTE.CODCLI(+) = NVL(N.CODCLINF, N.CODCLI)
+       and FI.CODIGO(+) = NVL(N.CODFILIALNF, N.CODFILIAL)
+       and M.DTCANCEL is null
+       and M.QTCONT > 0   
+   );
+   
 END PKG_CENTRAL_TRIBUTOS_CONSULTAS;
