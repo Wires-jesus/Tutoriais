bf139aa453a4f248bed1de122dc6ceb597f861a2
Jefferson Rodrigues <jefferson.drodrigues@totvs.com.br>
2025-08-12 16:50:36 -0300

    Criação da view VW_INT_C5_CCTCODIGOTRIBUTARIO

--- Migration/PROCESSO_CARGA_INT_C5_SEQUENCE/PROCESSO_CARGA_INT_C5_SEQUENCE.sql
+++ Migration/PROCESSO_CARGA_INT_C5_SEQUENCE/PROCESSO_CARGA_INT_C5_SEQUENCE.sql
@@ -88,4 +88,16 @@ BEGIN
      EXECUTE IMMEDIATE vScript;  
   END IF;   
   
+  BEGIN
+    SELECT COUNT(1)
+    INTO vnContSeq
+    FROM ALL_SEQUENCES A
+    WHERE A.SEQUENCE_NAME = 'DFSEQ_INT_C5_CCTCODIGOTRIBUTARIO';
+  END;
+  
+  IF vnContSeq = 0  THEN
+     vScript := ' CREATE SEQUENCE DFSEQ_INT_C5_CCTCODIGOTRIBUTARIO minvalue 1 maxvalue 99999999999999 start with 1 increment by 1 nocache';
+     EXECUTE IMMEDIATE vScript;  
+  END IF;  
+
 END;                
