507ec9e3161d6970f76b58647d3588163e713719
eddy.pereira <eddy.pereira@totvs.com.br>
2026-04-02 19:21:56 -0300

    hotfix/suporte-DDDOC-3940 - Criada migration que cria scripts na rotina 1458; - Nesta versão, foi incluida um script para gerar a mensagem de "Operação sujeita à redução linear de benefícios da LC 224/2025 (NF-e)";

---
+++ Migration/CADASTRO_SCRIPTS_1458/CADASTRO_SCRIPTS_1458.sql
@@ -0,0 +1,51 @@
+DECLARE
+  vCodMensagem number(10);
+
+  function getNextvalSequence return number is
+    vretorno number(10);
+  begin
+    select dfseq_pcmensagemadicional.nextval into vretorno from dual;
+    return vretorno;
+  end;
+
+BEGIN
+  vCodMensagem := getNextvalSequence();
+  ------------------Inicio: Operação sujeita à redução linear de benefícios da LC 224/2025 (NF-e)------------------
+  INSERT INTO PCMENSAGEMADICIONAL
+    (CODMENSAGEM, DESCRICAO, MOVIMENTO, SQL, MENSAGEMATIVA, OBRIGATORIA)
+  VALUES
+    (vCodMensagem,
+     'Operação sujeita à redução linear de benefícios da LC 224/2025 (NF-e)',
+     'SA',
+     'SELECT DISTINCT MENSAGEM01
+  FROM (SELECT ''Operação sujeita à redução linear de benefícios da LC 224/2025'' MENSAGEM01
+        FROM PCNFSAID
+          ,PCMOV
+          ,PCTRIBPISCOFINSVIGENCIA
+       WHERE PCNFSAID.NUMTRANSVENDA = PCMOV.NUMTRANSVENDA
+         AND PCMOV.CODSITTRIBPISCOFINS = PCTRIBPISCOFINSVIGENCIA.CODTRIBPISCOFINS
+         AND PCMOV.DTMOV BETWEEN PCTRIBPISCOFINSVIGENCIA.DTINICIO AND PCTRIBPISCOFINSVIGENCIA.DTFINAL
+         AND PCTRIBPISCOFINSVIGENCIA.LC22425 = ''S''
+         AND PCNFSAID.NUMTRANSVENDA = :NUM_TRANSACAO
+      UNION
+      SELECT ''Operação sujeita à redução linear de benefícios da LC 224/2025'' MENSAGEM01
+        FROM PCNFSAID
+          ,PCMOV
+          ,PCTRIBPISCOFINS
+       WHERE PCNFSAID.NUMTRANSVENDA = PCMOV.NUMTRANSVENDA
+         AND PCMOV.CODSITTRIBPISCOFINS = PCTRIBPISCOFINS.CODTRIBPISCOFINS
+         AND PCTRIBPISCOFINS.LC22425 = ''S''
+         AND PCNFSAID.NUMTRANSVENDA = :NUM_TRANSACAO)',
+     'S',
+     'N');
+
+  INSERT INTO PCMENSAGEMADICIONALITENS
+    (CODMENSAGEM, ORDEM, TEXTO)
+  VALUES
+    (vCodMensagem, 0, '[MENSAGEM01]');
+  ------------------Fim: Operação sujeita à redução linear de benefícios da LC 224/2025 (NF-e)------------------
+
+  
+  
+  COMMIT;
+END;
