20e5864f8632147ee58b2b6f55b41ac1c8fb7f31
Jefferson Rodrigues <jefferson.drodrigues@totvs.com.br>
2026-05-08 15:27:06 -0300

    Adequação a busca do código de beneficio fiscal

--- Function/FNC_INT_C5_CBENEF/FNC_INT_C5_CBENEF.sql
+++ Function/FNC_INT_C5_CBENEF/FNC_INT_C5_CBENEF.sql
@@ -1,20 +1,23 @@
 CREATE OR REPLACE FUNCTION FNC_INT_C5_CBENEF(pUF         CHAR,
                                              pCFOP       NUMBER,
-                                             pSittribut  VARCHAR2)
+                                             pSittribut  VARCHAR2,
+                                             pCodSt      VARCHAR2)
   RETURN VARCHAR2
   IS
     vCBenef  VARCHAR2(10);
-  BEGIN
-
-    SELECT  v.codigobeneficio
-      INTO  vCBenef
-      FROM  pccodigobeneficiofiscalvinculo v,
-            pccodigobeneficiofiscal        c
-     WHERE  v.codigobeneficio = c.codigobeneficio
-       AND  v.ufdestino = pUF
-       AND  v.sittribut = pSittribut
-       AND  v.codfiscal = pCFOP
-       AND  ROWNUM = 1;
-    
-    RETURN(vCBenef);
-END;
+BEGIN
+  SELECT v.codigobeneficio
+    INTO vCBenef
+    FROM pccodigobeneficiofiscalvinculo v
+    JOIN pccodigobeneficiofiscal c ON v.codigobeneficio = c.codigobeneficio
+    WHERE v.ufdestino = pUF
+      AND v.sittribut = pSittribut
+      AND v.codfiscal = pCFOP
+      AND v.figuratributaria = pCodSt
+      AND ROWNUM = 1;
+      
+  RETURN vCBenef;
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    RETURN NULL;
+END FNC_INT_C5_CBENEF;
