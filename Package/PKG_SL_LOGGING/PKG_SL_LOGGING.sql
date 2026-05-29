2fcdb1642943582bb0b36f645d3ecf0630a07e40
Pedro Henrique Franca Vieira <pedro.hvieira@totvs.com.br>
2026-05-29 11:24:45 +0000

    Updated PKG_SL_LOGGING.sql revert

--- Package/PKG_SL_LOGGING/PKG_SL_LOGGING.sql
+++ Package/PKG_SL_LOGGING/PKG_SL_LOGGING.sql
@@ -2,7 +2,7 @@
 -- Descrição: Pacote para gravação de logs de execução em TBL_SL_LOGGING.
 -- Utiliza transação autônoma para garantir o log mesmo em caso de rollback.
 
-CREATE OR REPLACE PACKAGE PKG_SL_LOGGING AUTHID CURRENT_USER AS
+CREATE OR REPLACE PACKAGE PKG_SL_LOGGING AS
 
     PROCEDURE WRITE_LOG(
         p_log_level       IN VARCHAR2,
