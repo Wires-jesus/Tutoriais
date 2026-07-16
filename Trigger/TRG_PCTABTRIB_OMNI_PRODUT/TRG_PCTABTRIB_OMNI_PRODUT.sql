20c7cf5ebea6f58052228133f5d9c2c5be4f2d4c
Bruna <bruna.balves@totvs.com.br>
2026-07-16 10:52:11 -0300

    [DDWMISSI-4782] | Otimização das Triggers OMNI_PRODUT e Correção de Performance

--- Trigger/TRG_PCTABTRIB_OMNI_PRODUT/TRG_PCTABTRIB_OMNI_PRODUT.sql
+++ Trigger/TRG_PCTABTRIB_OMNI_PRODUT/TRG_PCTABTRIB_OMNI_PRODUT.sql
@@ -1,18 +1,37 @@
 CREATE OR REPLACE TRIGGER TRG_PCTABTRIB_OMNI_PRODUT
-    AFTER INSERT OR UPDATE OR DELETE
+    AFTER INSERT OR UPDATE OF CODST OR DELETE
     ON PCTABTRIB
     FOR EACH ROW
 DECLARE
-    v_codprod NUMBER;
+    v_codprod   NUMBER;
+    v_usa_omni  NUMBER := 0;
+    v_sql       VARCHAR2(1000);
 BEGIN
-    IF INSERTING THEN
-        v_codprod := :NEW.CODPROD;
-    ELSIF UPDATING THEN
+    IF INSERTING OR UPDATING THEN
         v_codprod := :NEW.CODPROD;
     ELSIF DELETING THEN
         v_codprod := :OLD.CODPROD;
-    end if;
-    UPDATE PCPRODUT
-    SET DESCRICAO = DESCRICAO
-    WHERE CODPROD = v_codprod;
-end;
+    END IF;
+
+    v_sql := 'SELECT 1 ' ||
+             'FROM PCINTEGRACAODADOSEMPRESA a, PCINTEGRACAOFLUXOEXECUCAO b ' ||
+             'WHERE a.nome = ''PDVSYNC'' ' ||
+             'AND b.DESCRICAO = ''Produto'' ' ||
+             'AND b.ativo = ''S'' ' ||
+             'AND ROWNUM = 1';
+
+    BEGIN
+        EXECUTE IMMEDIATE v_sql INTO v_usa_omni;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            v_usa_omni := 0;
+        WHEN OTHERS THEN
+            v_usa_omni := 0;
+    END;
+
+    IF v_usa_omni > 0 THEN
+        UPDATE PCPRODUT
+        SET DTULTALTER = SYSDATE
+        WHERE CODPROD = v_codprod;
+    END IF;
+END;
