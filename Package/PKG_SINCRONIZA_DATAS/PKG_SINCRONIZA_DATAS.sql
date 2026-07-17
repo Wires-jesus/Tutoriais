bf74fead367257039d1820f82184b727c561271d
Wires-jesus <114195488+Wires-jesus@users.noreply.github.com>
2026-07-17 11:48:07 -0300

    Delete Package directory

--- Package/PKG_SINCRONIZA_DATAS/PKG_SINCRONIZA_DATAS.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE PACKAGE PKG_SINCRONIZA_DATAS AS
-    PROCEDURE sincronizar_datas_saida(p_data_inicio IN DATE);
-    
-    FUNCTION contar_discrepancias(p_data_inicio IN DATE) RETURN NUMBER;
-END PKG_SINCRONIZA_DATAS;
